9cf9ecf5d95d8069512c3ab8df860c72d242011f
wani <oen142@naver.com>
2020-07-01 11:08:28 +0900

    react MultiLoad

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Hello from "./Hello";
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
+import InputSample from "./InputSample";
+import InputMultiSample from "./InputMultiSample";
+import UserList from "./UserList";
 
 function App() {
+
+    const users = [
+        {
+            id: 1,
+            username: 'kim',
+            email: 'naver.com'
+        },
+        {
+            id: 2,
+            username: 'Lee',
+            email: 'kakao.com'
+        },
+        {
+            id: 3,
+            username: 'park',
+            email: 'uah.com'
+        }
+    ];
+    const nextId = useRef(4);
+
+    const onCreate = () => {
+        console.log(nextId.current);// 4
+        nextId.current += 1;
+    }
+
     return (
         <>
             <Counter/>
@@ -12,6 +40,12 @@ function App() {
                 {/* ={true} 생략가능*/}
                 <Hello name="react" color="red"/>
             </Wrapper>
+
+            <InputSample/>
+            <InputMultiSample/>
+
+
+            <UserList users={users}/>
         </>
     );
 }
